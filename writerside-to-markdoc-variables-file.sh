9b7b576d7543dbc2e78418afe80b212347c9368b
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2025-09-17 01:07:15 -0400

    Fixed the convert-writerside-vars-to-markdoc-vars.sh so it correctly filters out duplicates and creates correctly formed JSON.

--- writerside-to-markdoc-variables-file.sh
+++ writerside-to-markdoc-variables-file.sh
@@ -2,8 +2,9 @@
 #
 # This script reads in an input Markdoc variable file and converts it to a Markdoc variable file named markdoc_variables.json.
 
-# Initialize the JSON file by emptying it.
+# Initialize the JSON and temp file by emptying them.
 > markdoc_variables.json
+> tmp.txt
 
 # Process the input file and extract variable names to an array
 # called VARIABLE_NAMES
@@ -16,13 +17,30 @@ mapfile -t VARIABLE_VALUES < <(cat $1 | \
 awk ' BEGIN {FS="\""} /<var name="/ {print $4}' )
 
 # Create the static content at the top of the JSON file.
-echo "{variables:" >> markdoc_variables.json
+echo "{\"variables\":" >> markdoc_variables.json
 echo -e "\t{" >> markdoc_variables.json
 
-# Write each value of the variable name and variable value to
-# the JSON file.
-for (( i=0; i<"${#VARIABLE_NAMES[@]}"; i++ )); do
-    echo -e "\t\t'${VARIABLE_NAMES[$i]}': '${VARIABLE_VALUES[$i]}'," >> markdoc_variables.json
+VARIABLE_NAMES_AND_VALUES=()
+
+# Create a third array where each entry is a colon-separated pair
+# from the first two arrays, so we can sort and eliminate duplicates.
+
+for (( counter=0; counter<${#VARIABLE_NAMES[@]}; counter++ )); do
+    VARIABLE_NAMES_AND_VALUES[$counter]="${VARIABLE_NAMES[$counter]}:${VARIABLE_VALUES[$counter]}"
+done
+
+# Create a fourth array that is the sorted version of the third array
+# where duplicates have been removed, and remove any empty values that snuck in during processing.
+printf "%s\n" "${VARIABLE_NAMES_AND_VALUES[@]}" | sort -u | xargs -I {} echo \"{}\" >> tmp.txt
+mapfile -t SORTED_WITHOUT_DUPES < <(cat tmp.txt | grep -v -P "^\"$")
+
+# Process the array, writing to the JSON file, splitting on the field separator, which is the colon, so we can add quotes and spaces and a trailing comma as needed to form proper JSON. The first if/fi clause handles every entry except the very last one, which should not have a closing comma.
+for (( i=0; i<"${#SORTED_WITHOUT_DUPES[@]}"; i++ )); do
+    if (( i < "${#SORTED_WITHOUT_DUPES[@]}" - 1 )) ; then
+        echo "${SORTED_WITHOUT_DUPES[$i]}" | awk -F ":" '{ print "\t\t" $1 "\": \"" $2 "," }' >> markdoc_variables.json
+    else
+        echo "${SORTED_WITHOUT_DUPES[$i]}" | awk -F ":" '{ print "\t\t" $1 "\": \"" $2 }' >> markdoc_variables.json
+    fi
 done
 
 # Write the static closing text to the file.
